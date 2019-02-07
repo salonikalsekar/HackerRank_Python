e51d6a2ac65e369a99c26333a3bf526df0834b2e
salonikalsekar <saloniskalsekar@gmail.com>
2019-02-06 21:47:56 -0700

    basicDataTypes

---
+++ runnerup-list.py
@@ -0,0 +1,30 @@
+# Given the participants' score sheet for your University Sports Day, you are required to find the runner-up score. You are given scores. Store them in a list and find the score of the runner-up.
+
+# Input Format
+
+# The first line contains . The second line contains an array   of  integers each separated by a space.
+
+# Output Format:
+
+# Print the runner-up score.
+
+# Sample Input 0
+
+# 5
+# 2 3 6 6 5
+# Sample Output 0
+
+# 5
+# Explanation 0
+
+# Given list is [2,3,6,6,5] . The maximum score is 6, second maximum is 5. Hence, we print 5 as the runner-up score.
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+
+for i in sorted(arr, reverse=True):
+    if(i < max(arr)):
+        print(i)
+        break
