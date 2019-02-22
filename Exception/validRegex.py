0090beb45c446061c843b98049f6bdf3b633a37c
salonikalsekar <saloniskalsekar@gmail.com>
2019-02-21 20:57:47 -0700

    exceptions

---
+++ Exception/validRegex.py
@@ -0,0 +1,37 @@
+# You are given a string . 
+# Your task is to find out whether  is a valid regex or not.
+
+# Input Format
+
+# The first line contains integer , the number of test cases. 
+# The next  lines contains the string .
+
+# Constraints
+
+
+# Output Format
+
+# Print "True" or "False" for each test case without quotes.
+
+# Sample Input
+
+# 2
+# .*\+
+# .*+
+# Sample Output
+
+# True
+# False
+# Explanation
+
+# .*\+ : Valid regex. 
+# .*+: Has the error multiple repeat. Hence, it is invalid.
+
+import re
+for _ in range(int(input())):
+    result = True
+    try:
+        reg = re.compile(input())
+    except re.error:
+        result = False
+    print(result)
