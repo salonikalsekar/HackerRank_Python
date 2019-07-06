1a17387b277fbcd7c7811ac622b7127f6829f0cd
Saloni Kalsekar <saloniskalsekar@gmail.com>
2019-07-06 18:58:26 -0400

    string_formatting

--- Strings/stringFormatting.py
+++ Strings/stringFormatting.py
@@ -40,3 +40,13 @@
 #    17    21    11 10001     
 
 
+def print_formatted(number):
+    for i in range(1, number + 1):
+        width = len("{0:b}".format(number))
+        print("{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}".format(i, width=width))
+
+
+
+if __name__ == '__main__':
+    n = int(input())
+    print_formatted(n)
