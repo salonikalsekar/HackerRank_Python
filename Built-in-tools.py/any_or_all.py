bdb5621ffc8ac89c0c9c4677dfb6ee3ceb98fc0f
Saloni Kalsekar <saloniskalsekar@gmail.com>
2019-07-12 23:14:55 -0600

    zipped

---
+++ Built-in-tools.py/any_or_all.py
@@ -0,0 +1,24 @@
+# any()
+# This expression returns True if any element of the iterable is true.
+# If the iterable is empty, it will return False.
+#
+# Code
+#
+# >>> any([1>0,1==0,1<0])
+# True
+# >>> any([1<0,2<1,3<2])
+# False
+# all()
+# This expression returns True if all of the elements of the iterable are true. If the iterable is empty, it will return True.
+#
+# Code
+#
+# >>> all(['a'<'b','b'<'c'])
+# True
+# >>> all(['a'<'b','c'<'b'])
+# False
+# Task
+#
+# You are given a space separated list of integers. If all the integers are positive
+# then you need to check if any integer is a palindromic integer.
+
